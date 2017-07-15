af9b299da4a865680bd8a6a1768231a81969da1b
lyzl <lyzl5764@hotmail.com>
2017-07-16 04:34:32 +0800

    add diagonal_unit to solve diagonal Sudoku

--- solution.py
+++ solution.py
@@ -11,7 +11,8 @@ boxes = cross(rows,cols)
 row_units = [cross(r, cols) for r in rows]
 column_units = [cross(rows, c) for c in cols]
 square_units = [cross(rs, cs) for rs in ('ABC','DEF','GHI') for cs in ('123','456','789')]
-unit_list = row_units + column_units + square_units
+diagonal_units = [[rows[i] + cols[i] for i in range(9)], [rows[i] + cols[8 - i] for i in range(9)]]
+unit_list = row_units + column_units + square_units + diagonal_units
 units = dict((s, [u for u in unit_list if s in u]) for s in boxes)
 peers = dict((s, set(sum(units[s],[]))-set([s])) for s in boxes)
 
